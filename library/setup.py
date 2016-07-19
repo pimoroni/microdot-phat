2b8392d4bf6b2aa1f076a77c729dfb4791cfa72b
RogueM <chaosgen6@googlemail.com>
2016-07-19 18:57:51 +0100

    PyPi resources straightened

--- library/setup.py
+++ library/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright (c) 2014 Pimoroni
+Copyright (c) 2016 Pimoroni
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -37,18 +37,19 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Topic :: Software Development',
                'Topic :: System :: Hardware']
 
-setup(name		= 'microdotphat',
-	version		= '0.1.2',
-	author		= 'Philip Howard',
-	author_email	= 'phil@pimoroni.com',
-	description	= 'Micro Dot pHAT Driver',
-	long_description= open('README.md').read() + open('CHANGELOG.txt').read(),
-	license		= 'MIT',
-	keywords	= 'Raspberry Pi IoT Displays LCD LED',
-	url		= 'http://www.pimoroni.com',
-	classifiers	= classifiers,
-	py_modules	= [],
-	packages	= ['microdotphat'],
-	include_package_data = True,
-	install_requires= []
+setup(
+    name            = 'microdotphat',
+    version         = '0.1.2',
+    author          = 'Philip Howard',
+    author_email    = 'phil@pimoroni.com',
+    description     = 'Micro Dot pHAT Driver',
+    long_description= open('README.txt').read() + open('CHANGELOG.txt').read(),
+    license         = 'MIT',
+    keywords        = 'Raspberry Pi IoT Displays LCD LED',
+    url             = 'http://www.pimoroni.com',
+    classifiers     = classifiers,
+    py_modules      = [],
+    packages        = ['microdotphat'],
+    include_package_data = True,
+    install_requires= []
 )
